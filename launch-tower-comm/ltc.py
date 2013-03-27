59aac55a8756491318882d8234d7432858c4de55
kebno <jboyle@cecs.pdx.edu>
2013-03-27 12:34:15 -0700

    adding empty ltc kivy app

--- launch-tower-comm/ltc.py
+++ launch-tower-comm/ltc.py
@@ -14,7 +14,6 @@ from kivy.config import ConfigParser
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label  import Label
@@ -22,76 +21,41 @@ from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
 
 ########### KIVY Setup ############
 
-class KvPhDemo(FloatLayout):
-    ''' Loaded from the kv lang file
-    '''
+class LTC(FloatLayout):
+    # Loaded from the kv lang file
+    pass
+    
+#class StandardWidgets(FloatLayout):
 
-class ControlPanel(BoxLayout):
-    '''Loaded from the kv lang file.
-    '''
+    #value = NumericProperty(0)
 
-class OutDevice(BoxLayout):
-    ''' Loaded from the kv lang file.
-    '''
-    # 'self.__init__' didn't work to do what 'set_properties' does
-    # b/c I found I can't call object attributes defined within the
-    # kv lang file until apparently AFTER the object has been initialized.
-    # That isn't to say that there is a more natural way of doing this
-    # that is provided by Kivy.
+    #def __init__(self, **kwargs):
+        #super(StandardWidgets, self).__init__(**kwargs)
+        #Clock.schedule_interval(self.increment_value, 1 / 30.)
 
-    def set_properties(self, name, iotype, ioindex):
-        self.device_label.text = name + ' ' + str(ioindex)# an attribute from kv file
-        self.iotype = iotype
-        self.ioindex = ioindex
+    #def increment_value(self, dt):
+        #self.value += dt
 
-        # Explicitly set outputs to OFF (good habit in control situations)
-        # Just as easily could read the current status instead.
-        self.status_ind.text = 'OFF'
-        interfaceKit.setOutputState(self.ioindex, False)
+class LTCApp(App):
 
-        # Check connection status every half second.
-        Clock.schedule_interval(self.check_connection, 0.5)
-
-    def check_connection(self, instance):
-        # 'ik_attached' is set by the phidgets 'interfaceKitDetached' and
-        # 'interfaceKitAttached' event handlers
-        self.conn_ind.text = 'Connected' if bool(ik_attached) else 'disconnected'
-
-    def toggle_state(self, state):
-        ledstate = interfaceKit.getOutputState(self.ioindex)
-        ledstate = not ledstate
-        interfaceKit.setOutputState(self.ioindex, ledstate)
-        self.status_ind.text = 'OFF' if state=='normal' else 'ON'
-
-
-class KvPhDemoApp(App):
+    #~ def __init__(self, **kwargs):
+        #~ super(App, self).__init__(**kwargs)
 
     def build(self):
         # The 'build' method is called when the object is run.
 
-        kvphdemo = KvPhDemo()
-        controlpanel = ControlPanel()
-
-        #This LED setup may seem repetitive in this simple example.
-        # It would be useful in more complex situations
-        led1 = OutDevice()
-        led4 = OutDevice()
-        led6 = OutDevice()
-
-        led1.set_properties(name='LED', iotype='output', ioindex=1)
-        led4.set_properties(name='LED', iotype='output', ioindex=4)
-        led6.set_properties(name='LED', iotype='output', ioindex=6)
-
-        controlpanel.add_widget(led1)
-        controlpanel.add_widget(led4)
-        controlpanel.add_widget(led6)
-
-        kvphdemo.content.add_widget(controlpanel) # 'content' is a reference to a
+        root = BoxLayout(spacing=10)
+        btn1 = Button(text='Hello', size_hint=(.7, 1))
+        btn2 = Button(text='World', size_hint=(.3, 1))
+        root.add_widget(btn1)
+        root.add_widget(btn2)
+        
+        ltc = LTC()
+        ltc.content.add_widget(root) # 'content' is a reference to a
                                                   # layout placeholder in the
                                                   # kv lang file
-        return kvphdemo
+        return ltc
 
 
 if __name__ == '__main__':
-    interfaceKit = setup_interfaceKit()
-    KvPhDemoApp().run()
+    LTCApp().run()
