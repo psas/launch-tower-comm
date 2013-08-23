63f3c32b693dbbf0d63f0bb03e45013d63fe3e91
TheoH <Theo0x48@gmail.com>
2013-08-23 02:32:28 -0700

    Refactored ltcctrl

--- launch-tower-comm/ltcctrl.py
+++ launch-tower-comm/ltcctrl.py
@@ -9,6 +9,7 @@ from Phidgets.PhidgetException import PhidgetException
 
 from ltcbackend import LTCbackend
 
+
 class LTCButton(Button):
     def _do_press(self):
         pass
@@ -16,129 +17,125 @@ class LTCButton(Button):
     def _do_release(self):
         pass
 
+
 class LTCAccordionItem(AccordionItem):
     def on_touch_down(self, touch):
         if not self.collide_point(*touch.pos):
             return
         return super(AccordionItem, self).on_touch_down(touch)
 
+
 class IgnitionPopup(Popup):
+    ignition_abort_timeout = 10
+
     def __init__(self, ignite=lambda: None, abort=lambda: None, **kwargs):
         self.ignite = ignite
         self.abort = abort
         super(IgnitionPopup, self).__init__(auto_dismiss=False, **kwargs)
 
-    def ignitefunc(self):
-        self.ignite()
+    def on_button_ignite(self):
+        try:
+            Clock.schedule_once(self.abort, self.ignition_abort_timeout)
+            self.ignite(True)
+        except PhidgetException:
+            self.abort()
+            self.set_state('Phidget Call Failed')
+
 
 class LTCctrl(Accordion):
     # TODO move Call Failed and Nominal state displays to backend
-    # LTCctrl should only issue ARM and ARM -> Nominal|Abort Failed changes
+    # LTCctrl should only issue ARM and ARM -> Nominal changes
     # because they're the only state changes it has a say in.
 
     # TODO: checks that buttons are displaying the right thing?
 
-    ignition_abort_timeout = 10
-
     def __init__(self, ignite=lambda x: None, shorepower=lambda x: None, state=lambda x: None, **kwargs):
-        # setup gui
-        super(LTCctrl, self).__init__(**kwargs)
-        self.unarmed.collapse = False
-        self.popup = IgnitionPopup(self._on_popup_ignite)
-
         # setup callbacks
         self.ignite = ignite
         self.shorepower = shorepower
         self.set_state = state
-        # setup internal state. The button states could be used, but this seems
-        # safer becuase the buttons aren't always under our explicit control
-        # (e.g. they're from Kivy)
-        self._shorepower_state = None
-        self._ignition_state = None
+        # setup internal state.
+        self.state_shorepower = None
+        self.state_ignition = None
+        # setup gui
+        self.popup = IgnitionPopup(ignite, self.abort)
+        super(LTCctrl, self).__init__(**kwargs)
+        self.accordian_unarmed.collapse = False
 
 
     def on_shorepower(self, event):
         """Callback function to set shorepower buttons state"""
-        # This function should be the only place where the shorepower buttons
-        # are set.
+        # This function is the only place where the shorepower buttons are set
         if event.state is True:
-            self.shorepower_button_on.state = 'down'
-            self.shorepower_button_off.state = 'normal'
-            if self._ignition_state is True:
-                # TODO: log/set state that shorepower was turned on while
-                # ignition was on
-                self._on_abort()
+            self.button_shorepower_on.state = 'down'
+            self.button_shorepower_off.state = 'normal'
+            if self.state_ignition is True:
+                # TODO: log that shorepower was turned on while ignition is on
+                self.abort()
         elif event.state is False:
-            self.shorepower_button_on.state = 'normal'
-            self.shorepower_button_off.state = 'down'
+            self.button_shorepower_on.state = 'normal'
+            self.button_shorepower_off.state = 'down'
         else:
             raise TypeError
-        self._shorepower_state = event.state
+
+        self.state_shorepower = event.state
 
     def on_ignite(self, event):
         """Callback function to set the ignite button state"""
-        # This function should be the only place where the ignite button is set
+        # This function is the only place where the ignite button is set
         if event.state is True:
-            self.ignite_button.state = 'down'
-            self._ignition_state = True
+            self.button_ignite.state = 'down'
+            self.state_ignition = True
             # if ignite happens showing it takes precedence over everything
-            self.armed.collapse = False
+            self.accordion_armed.collapse = False
             self.popup.dismiss()
         elif event.state is False:
-            self.ignite_button.state = 'normal'
-            self.abort_button.state = 'normal'
-            # self.arm depends on self._ignition_state being correct
-            self._ignition_state = False
+            self.button_ignite.state = 'normal'
+            self.button_abort.state = 'normal'
+            Clock.unschedule(self.abort)
+            # self.arm depends on self.state_ignition being correct
+            self.state_ignition = False
             self.arm(False)
         else:
             raise TypeError
 
-    def _on_popup_ignite(self):
-        try:
-            self.ignite(True)
-            Clock.schedule_once(self._on_abort, self.ignition_abort_timeout)
-        except PhidgetException:
-            self.on_abort()
-            self.set_state('Phidget Call Failed')
-
     def arm(self, state):
         if state is True:
-            if self._shorepower_state is False:
-                self.armed.collapse = False
+            if self.state_shorepower is False:
+                self.accordion_armed.collapse = False
                 self.set_state('ARMED')
             # TODO: else log that arm was attempted with sp true
         elif state is False:
-            if self._ignition_state is False:
-                self.unarmed.collapse = False
-                self.set_state('Nominal')
+            if self.state_ignition is False:
+                self.accordion_unarmed.collapse = False
+                self.set_state('Nominal')  # TODO: this should be disarm state
             else:
                 raise RuntimeError("Attempt to disarm was made while ignition relay was closed")
         else:
             raise TypeError
 
-    def _ignition_popup(self):
-        if self.abort_button.state == 'down':
-            return
+    def abort(self, event=None):
+        Clock.unschedule(self.abort)
+        if self.state_ignition == False:
+            self.arm(False)
+        else:
+            self.button_abort.state = 'down'
+            try:
+                self.ignite(False)
+            except PhidgetException:
+                self.button_abort.state = 'normal'
+                # TODO: log abort failed
+                self.set_state('Phidget Call Failed')
 
-        if self._ignition_state is True:
-            self._on_abort()
+    def on_button_ignite(self):
+        if self.button_abort.state == 'down':
+            pass  # TODO: log that ignite can't happen becuase abort is in progress
+        elif self.state_ignition is True:
+            self.abort()
         else:
             self.popup.open()
 
-    def _on_abort(self, event=None):
-        Clock.unschedule(self._on_abort)
-        if self._ignition_state == False:
-            self.abort_button.state = 'normal'
-            self.arm(False)
-            return
-
-        self.abort_button.state = 'down'
-        try:
-            self.ignite(False)
-        except PhidgetException:
-            self.set_state('Phidget Call Failed')
-
-    def _on_shorepower_button(self, state):
+    def on_button_shorepower(self, state):
         try:
             self.shorepower(state)
             self.set_state('Nominal')
