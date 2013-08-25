33401770b3d8b813d2cb8117d3ab1fa258ac5563
TheoH <Theo0x48@gmail.com>
2013-08-24 21:07:50 -0700

    Fixed race between popup closing and disarming

--- launch-tower-comm/ltcctrl.py
+++ launch-tower-comm/ltcctrl.py
@@ -28,22 +28,22 @@ class LTCAccordionItem(AccordionItem):
 class IgnitionPopup(Popup):
     ignition_abort_timeout = 10
 
-    def __init__(self, ignite=lambda: None, abort=lambda: None, **kwargs):
+    def __init__(self, ignite=lambda: None, abort=lambda: None, state=[], **kwargs):
         self.ignite = ignite
         self.abort = abort
+        self.state = state
         super(IgnitionPopup, self).__init__(auto_dismiss=False, **kwargs)
 
     def on_button_ignite(self):
         try:
             Clock.schedule_once(self.abort, self.ignition_abort_timeout)
             self.ignite(True)
+            self.state['popup_abort_lockin'] = True
         except PhidgetException:
             self.abort()
 
 
 class LTCctrl(Accordion):
-    # TODO move Call Failed and Nominal state displays to backend because
-    # ltcctrl doesn't actually control these things
 
     def __init__(self, ignite=lambda x: None, shorepower=lambda x: None, status=lambda x: None, **kwargs):
         # setup callbacks
@@ -51,9 +51,10 @@ class LTCctrl(Accordion):
         self.shorepower = shorepower
         self.set_status = status
         # setup internal state
-        self.state_shorepower = None
-        self.state_ignition = None
-        self.state_abort = None
+        self.state['shorepower'] = None
+        self.state['ignition'] = None
+        self.state['abort'] = None
+        self.state['popup_abort_lockin'] = None
         # nothing explicitly depends on the arm state
         # setup GUI
         self.popup = IgnitionPopup(ignite, self.abort)
@@ -67,7 +68,7 @@ class LTCctrl(Accordion):
         if event.state is True:
             self.button_shorepower_on.state = 'down'
             self.button_shorepower_off.state = 'normal'
-            if self.state_ignition is True:
+            if self.state['ignition'] is True:
                 # TODO: log that shorepower was turned on while ignition is on
                 self.abort()
         elif event.state is False:
@@ -76,36 +77,37 @@ class LTCctrl(Accordion):
         else:
             raise TypeError
 
-        self.state_shorepower = event.state
+        self.state['shorepower'] = event.state
 
     def on_ignite(self, event):
         """Callback function to set the ignite button state"""
         # This function is the only place where the ignite button is set
         if event.state is True:
             self.button_ignite.state = 'down'
-            self.state_ignition = True
+            self.state['ignition'] = True
+            self.state['popup_abort_lockin'] = False
             # if ignite happens showing it takes precedence over everything
             self.accordion_armed.collapse = False
             self.popup.dismiss()
         elif event.state is False:
             self.button_ignite.state = 'normal'
             self.button_abort.state = 'normal'
-            self.state_abort = False
+            self.state['abort'] = False
             Clock.unschedule(self.abort)
-            # self.arm depends on self.state_ignition being correct
-            self.state_ignition = False
+            # self.arm depends on self.state['ignition'] being correct
+            self.state['ignition'] = False
             self.arm(False)
         else:
             raise TypeError
 
     def arm(self, state):
         if state is True:
-            if self.state_shorepower is False:
+            if self.state['shorepower'] is False:
                 self.accordion_armed.collapse = False
                 self.set_status('ARMED')
             # TODO: else log that arm was attempted with sp true
         elif state is False:
-            if self.state_ignition is False:
+            if self.state['ignition'] is False:
                 self.accordion_unarmed.collapse = False
                 self.set_status('Disarmed')
             else:
@@ -118,22 +120,22 @@ class LTCctrl(Accordion):
         # when the ignite callback is called with true
         # This should be fixed
         Clock.unschedule(self.abort)
-        if self.state_ignition == False:
+        if self.state['ignition'] is False and self.state['popup_abort_lockin'] is not True:
             self.arm(False)
         else:
             self.button_abort.state = 'down'
-            self.state_abort = True
+            self.state['abort'] = True
             try:
                 self.ignite(False)
             except PhidgetException:
                 self.button_abort.state = 'normal'
-                self.state_abort = False
+                self.state['abort'] = False
                 self.set_status('Abort Failed')
 
     def on_button_ignite(self):
-        if self.state_abort is True:
+        if self.state['abort'] is True:
             pass  # TODO: log that ignite can't happen becuase abort is in progress
-        elif self.state_ignition is True:
+        elif self.state['ignition'] is True:
             self.abort()
         else:
             self.popup.open()
@@ -141,7 +143,6 @@ class LTCctrl(Accordion):
     def on_button_shorepower(self, state):
         try:
             self.shorepower(state)
-            self.set_status('Nominal')
         except PhidgetException:
             pass
 
