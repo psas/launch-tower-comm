8960ba1be1af576a6afb7ae3ce957216ca3b315e
Theo Hill <Theo0x48@gmail.com>
2026-07-11 00:44:45 -0700

    Separate Callback fanout from Phidget classes
    It would be nice if we didn't need to rely on Phidgets existing for
    managing callbacks, then we could potentially run the UI without any
    real hardware. I've done this here by turning the callbacks into a mixin
    class.
    
    For now this is purely a separation of concerns thing but this could be
    useful in the future (hinthint).

--- launch-tower-comm/ltcbackend.py
+++ launch-tower-comm/ltcbackend.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable
 from enum import Enum, unique
-from typing import Any, override
+from typing import Any, Self, override
 
 import ltclogger as log
 
@@ -16,9 +16,10 @@ from Phidget22.Phidget import Phidget
 from Phidget22.PhidgetException import PhidgetException
 
 
-class LTCPhidget(Phidget):
-    def __init__(self) -> None:
+class CallbackFanout[V]:
+    def __init__(self, name: str) -> None:
         super().__init__()
+        self.name = name
         # detach fires both on network disconnect and USB cable disconnect.
         # attach fires on open() and USB cable connect but not network reattach?
         self._callback: dict[str, list[Callable[..., None]]] = {
@@ -26,55 +27,66 @@ class LTCPhidget(Phidget):
             'detach': [],
             'error': [],
             'property': [],
+            'value': [],
         }
+        if isinstance(self, Phidget):
+            self.setOnAttachHandler(lambda _: self._on_attach())
+            self.setOnDetachHandler(lambda _: self._on_detach())
+            self.setOnErrorHandler(lambda _, code, desc: self._on_error(code, desc))
 
-        self.setOnAttachHandler(self._on_attach)
-        self.setOnDetachHandler(self._on_detach)
-        self.setOnErrorHandler(self._on_error)
-        self.setOnPropertyChangeHandler(self._on_property)
+            def on_property(self: Self, device: Phidget, name: str) -> None:
+                # Why the callback can't just give us the value I'll never know. Here
+                # we reconstruct the getter method for the associated property and then
+                # get that propterty. Its TOCTOU but I am unaware of a better way.
+                value = getattr(device, 'get' + name)()
+                self._on_property(name, value)
+
+            self.setOnPropertyChangeHandler(on_property)
+            if hasattr(self, "setOnSensorChangeHandler"):
+                self.setOnSensorChangeHandler(lambda _d, v, _u: self._on_value(v))
 
     def add_callback(self, cb: Callable[..., None], event_type: str) -> None:
-        cbname = f"{cb.__self__.__class__.__name__}.{cb.__name__}"
+        cbname = cb.__name__
+        if hasattr(cb, "__self__"):
+            cbname = f"{cb.__self__.__class__.__name__}.{cb.__name__}"
         log.debug(f"Adding {event_type:6} callback to {self.name}: {cbname}")
         self._callback[event_type].append(cb)
 
-    def _on_attach(self, _device: Phidget) -> None:
+    def _on_attach(self) -> None:
         log.verbose(f"{self.name} Attached")
         for cb in self._callback['attach']:
             cb()
 
-    def _on_detach(self, _device: Phidget) -> None:
+    def _on_detach(self) -> None:
         log.verbose(f"{self.name} Detached")
         for cb in self._callback['detach']:
             cb()
 
-    def _on_error(self, _device: Phidget, code: int, description: str) -> None:
+    def _on_error(self, code: int, description: str) -> None:
         log.error(f"{self.name} error {code}: {description}")
         for cb in self._callback['error']:
             cb(code)
 
-    def _on_property(self, _device: Phidget, name: str) -> None:
-        # Why the callback can't just give us the value I'll never know. Here
-        # we reconstruct the getter method for the associated property and then
-        # get that propterty. Its TOCTOU but I am unaware of a better way.
-        value = getattr(self, 'get' + name)()
+    def _on_property(self, name: str, value: Any) -> None:
         log.verbose(f"{self.name} property {name} -> {value}")
         for cb in self._callback['property']:
             cb(name, value)
 
-    def is_nominal(self, _val: Any) -> bool:
+    def _on_value(self, val: V) -> None:
+        for cb in self._callback['value']:
+            cb(val)
+
+    def is_nominal(self, _val: V) -> bool:
         return False
 
 
-class Relay(LTCPhidget, DigitalOutput):
+class Relay(CallbackFanout['Relay.State'], DigitalOutput):
     def __init__(self, name: str, devserial: int, channel: int, *, invert: bool = False) -> None:
-        super().__init__()
-        self._callback['value'] = []
+        super().__init__(name)
         self.setDeviceSerialNumber(devserial)
         self.setChannel(channel)
 
         self.unit = ''
-        self.name = name
         # invert == True ? Nominal closed : Nominal open
         self.invert = self.State.OFF if invert else self.State.ON
         self.channel = channel
@@ -84,15 +96,14 @@ class Relay(LTCPhidget, DigitalOutput):
         ON = True
         OFF = False
 
-    def _on_value(self, val: State) -> None:
-        for cb in self._callback['value']:
-            cb(val)
+        def __bool__(self) -> bool:
+            return self.value
 
     @override
-    def _on_attach(self, device: Phidget) -> None:
-        super()._on_attach(device)
-        state = self.getState()
-        self._on_value(self.State(state))
+    def _on_attach(self) -> None:
+        state = self.State(self.getState())
+        super()._on_attach()
+        self._on_value(state)
 
     @override
     def setState(self, state: State) -> None:
@@ -105,64 +116,44 @@ class Relay(LTCPhidget, DigitalOutput):
         return val != self.invert
 
 
-class TemperatureSensor(LTCPhidget, VoltageRatioInput):
+class TemperatureSensor(CallbackFanout[float], VoltageRatioInput):
     def __init__(self, name: str, devserial: int, channel: int, upper: float, lower: float) -> None:
-        super().__init__()
-        self._callback['value'] = []
-        self.setOnSensorChangeHandler(self._on_voltage)
+        super().__init__(name)
         self.setDeviceSerialNumber(devserial)
         self.setChannel(channel)
 
         self.unit = "C"
-        self.name = name
         self.upper = upper
         self.lower = lower
 
-        # Set the sensor type after attaching only
-        self.add_callback(self._set_type, 'attach')
-
-    def _on_voltage(self, _device: Phidget, ratio: float, _unit: str) -> None:
-        for cb in self._callback['value']:
-            cb(ratio)
+    @override
+    def _on_attach(self) -> None:
+        self.setSensorType(VoltageRatioSensorType.SENSOR_TYPE_1124)
+        super()._on_attach()
 
     @override
     def is_nominal(self, val: float) -> bool:
         return self.lower < val < self.upper
 
-    def _set_type(self) -> None:
-        try:
-            self.setSensorType(VoltageRatioSensorType.SENSOR_TYPE_1124)
-        except PhidgetException as e:
-            log.error(f"Error setting sensor type for {self.name}: {e}")
-            raise
 
-
-class VoltageSensor(LTCPhidget, VoltageInput):
+class VoltageSensor(CallbackFanout[float], VoltageInput):
     def __init__(self, name: str, devserial: int, channel: int, upper: float, lower: float) -> None:
-        super().__init__()
+        super().__init__(name)
         self.setDeviceSerialNumber(devserial)
         self.setChannel(channel)
-        self._callback['value'] = []
-
-        self.setOnSensorChangeHandler(self._on_voltage)
 
         self.unit = "V"
-        self.name = name
         self.upper = upper
         self.lower = lower
 
-        self.add_callback(self._set_type, 'attach')
-
-    def _on_voltage(self, _device: Phidget, value: float, _unit: str) -> None:
-        for cb in self._callback['value']:
-            cb(value)
+    @override
+    def _on_attach(self) -> None:
+        self.setSensorType(VoltageSensorType.SENSOR_TYPE_1135)
+        super()._on_attach()
 
     @override
     def is_nominal(self, val: float) -> bool:
         return self.lower < val < self.upper
-
-    def _set_type(self) -> None:
-        self.setSensorType(VoltageSensorType.SENSOR_TYPE_1135)
 
 
 class LTCError(Exception):
@@ -175,12 +166,16 @@ class LTCbackend:
     def __init__(self) -> None:
         log.info("Starting Backend")
         # Interface Kit 0/0/4 with relays - 1014
-        self.ignition = Relay('Ignition Relay', devserial=259173, channel=0, invert=True)
+        self.ignition: Relay | MockRelay = Relay(
+            'Ignition Relay', devserial=259173, channel=0, invert=True
+        )
         self.ignition.add_callback(self._on_attach, 'attach')
-        self.shore = Relay('Shorepower Relay', devserial=259173, channel=3, invert=True)
+        self.shore: Relay | MockRelay = Relay(
+            'Shorepower Relay', devserial=259173, channel=3, invert=True
+        )
 
         # Interface Kit 8/8/8 with sensors attached - 1018
-        self.sensors = [
+        self.sensors: list[Phidget] = [
             TemperatureSensor("Internal Temperature", 178346, 0, 40.0, 10.0),
             VoltageSensor("Ignition Battery", 178346, 1, 4.1 * 4, 3.6 * 4),
             VoltageSensor("Rocket Ready", 178346, 2, 5.0, 2.0),
@@ -192,6 +187,7 @@ class LTCbackend:
     def start(self) -> None:
         # Net.addServer('ltc', 'ltc.psas.lan', 5001, '', 0)
         Net.addServer('ltc', '10.0.0.1', 5661, '', 0)
+        # async open - wait for associated on_attach callback to fire before using devices
         self.ignition.open()
         self.shore.open()
         for sensor in self.sensors:
@@ -199,6 +195,7 @@ class LTCbackend:
 
     def _on_attach(self) -> None:
         # TODO: Handle Possible Error
+        # FIXME: phidget failsafe mode?
         self.ignite(Relay.State.OFF)
 
     def close(self) -> None:
